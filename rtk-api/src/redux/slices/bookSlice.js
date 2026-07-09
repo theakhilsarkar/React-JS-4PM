5311fbabd0d72879a74b0ae2b3e58184521e1c7e
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-07-09 16:02:12 +0530

    ...

--- rtk-api/src/redux/slices/bookSlice.js
+++ rtk-api/src/redux/slices/bookSlice.js
@@ -11,6 +11,11 @@ export const postBook = createAsyncThunk("books/post", async (book) => {
   return res.data;
 });
 
+export const deleteBook = createAsyncThunk("books/delete", async (id) => {
+  const res = await axios.delete("http://localhost:3000/books/" + id);
+  return res.data;
+});
+
 const bookSlice = createSlice({
   name: "books",
   initialState: {
@@ -19,28 +24,47 @@ const bookSlice = createSlice({
     error: null,
   },
   // sync
-  reducers: {},
+  reducers: {
+    searchBook: (state, action) => {
+      state.books = state.books.filter((book) => book.title == action.payload);
+    },
+  },
   // async = delay = network request
   // pending
   // fulfilled
   // rejected
   extraReducers: (builder) => {
-    builder.addCase(fetchBooks.pending, (state, action) => {
-      state.loading = true;
-    });
-    builder.addCase(fetchBooks.fulfilled, (state, action) => {
-      state.books = action.payload;
-      state.loading = false;
-    });
-    builder.addCase(fetchBooks.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error.message;
-    });
-    builder.addCase(postBook.fulfilled, (state, action) => {
-      state.loading = false;
-      state.books.push(action.payload);
-    });
+    builder
+      .addCase(fetchBooks.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.books = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(postBook.fulfilled, (state, action) => {
+        state.loading = false;
+        state.books.push(action.payload);
+      })
+      .addCase(deleteBook.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(deleteBook.fulfilled, (state, action) => {
+        state.loading = false;
+        state.books = state.books.filter(
+          (book) => book.id != action.payload.id,
+        );
+      })
+      .addCase(deleteBook.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
+export const { searchBook } = bookSlice.actions;
 export default bookSlice.reducer;
--- rtk-api/src/screens/Books.jsx
+++ rtk-api/src/screens/Books.jsx
@@ -1,23 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchBooks, postBook } from "../redux/slices/bookSlice.js";
+import {
+  deleteBook,
+  fetchBooks,
+  postBook,
+  searchBook,
+} from "../redux/slices/bookSlice.js";
 
 export default function Books() {
   const dispatch = useDispatch();
   const { books, loading, error } = useSelector((state) => state.books);
+  const titleRef = useRef("");
+  const authorRef = useRef("");
+  const searchRef = useRef("");
+
   useEffect(() => {
     dispatch(fetchBooks());
   }, []);
   return (
     <div>
       <h1>Books</h1>
-      <button
-        onClick={() =>
-          dispatch(postBook({ title: "America", author: "Captain" }))
-        }
-      >
-        Add Book
-      </button>
+      <div>
+        <input ref={searchRef} type="text" />
+        <button
+          onClick={() => {
+            dispatch(searchBook(searchRef.current.value));
+          }}
+        >
+          Search
+        </button>
+        <button onClick={() => dispatch(fetchBooks())}>Reset</button>
+      </div>
+      <div>
+        <input type="text" ref={titleRef} />
+        <input type="text" ref={authorRef} />
+        <button
+          onClick={() =>
+            dispatch(
+              postBook({
+                title: titleRef.current.value,
+                author: authorRef.current.value,
+              }),
+            )
+          }
+        >
+          Add Book
+        </button>
+      </div>
+
       <div>
         {loading ? (
           <p>Loading.........</p>
@@ -26,6 +56,13 @@ export default function Books() {
             <div key={i}>
               <p>{book.title}</p>
               <i>{book.author}</i>
+              <button
+                onClick={() => {
+                  dispatch(deleteBook(book.id));
+                }}
+              >
+                -
+              </button>
             </div>
           ))
         ) : (
